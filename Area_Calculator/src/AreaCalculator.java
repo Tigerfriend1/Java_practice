2d4b49609f2868b74ab2400017492817fd6cc14e
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-09-27 15:40:04 +0900

    feat: use switch

--- Area_Calculator/src/AreaCalculator.java
+++ Area_Calculator/src/AreaCalculator.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,33 +8,70 @@ public class AreaCalculator {
 
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
-//        List<String> list = new ArrayList<>();
-//        list.add(string.split(" "));
-        //String[] str1 = string.split(" ");
-        while (!string.equals("Quit")) {
-            String[] str1 = string.split(" ");
-            if (str1[0].equalsIgnoreCase("Triangle")) {
+        enum Shape {TRIANGLE, RECTANGLE, CIRCLE}
 
-                double base = Float.parseFloat(str1[1]);
-                double height = Float.parseFloat(str1[2]);
-                System.out.printf("Area of Triangle: %.2f\n",base*height*0.5);
-            }
-            else if(str1[0].equalsIgnoreCase("Rectangle")){
-                double width = Float.parseFloat(str1[1]);
-                double height = Float.parseFloat(str1[2]);
-                System.out.printf("Area of Rectangle: %.2f\n",width*height);
-            }
-            else if(str1[0].equalsIgnoreCase("Circle")){
-                double radius = Float.parseFloat(str1[1]);
-                System.out.printf("Area of Circle: %.2f\n",radius*radius*Math.PI);
-            }
-            else {
-                System.out.println("Invalid\n");
+        while (!string.equalsIgnoreCase("Quit")) {
+            String[] str1 = string.split(" ");
+            String shape = str1[0].toLowerCase();
+
+            switch (shape) {
+                case "triangle":
+                    double base = Double.parseDouble(str1[1]);
+                    double height = Double.parseDouble(str1[2]);
+                    System.out.printf("Area of Triangle: %.2f\n", base * height * 0.5);
+                    break;
+                case "rectangle":
+                    double width = Double.parseDouble(str1[1]);
+                    height = Double.parseDouble(str1[2]);
+                    System.out.printf("Area of Rectangle: %.2f\n", width * height);
+                    break;
+                case "circle":
+                    double radius = Double.parseDouble(str1[1]);
+                    System.out.printf("Area of Circle: %.2f\n", radius * radius * Math.PI);
+                    break;
+                default:
+                    System.out.println("Invalid\n");
+                    break;
             }
             string = scanner.nextLine();
         }
+
         System.out.println("Bye\n");
         scanner.close();
-
     }
 }
+
+
+
+
+
+
+
+
+
+//        while (!string.equals("Quit")) {
+//            String[] str1 = string.split(" ");
+//            if (str1[0].equalsIgnoreCase("Triangle")) {
+//
+//                double base = Float.parseFloat(str1[1]);
+//                double height = Float.parseFloat(str1[2]);
+//                System.out.printf("Area of Triangle: %.2f\n",base*height*0.5);
+//            }
+//            else if(str1[0].equalsIgnoreCase("Rectangle")){
+//                double width = Float.parseFloat(str1[1]);
+//                double height = Float.parseFloat(str1[2]);
+//                System.out.printf("Area of Rectangle: %.2f\n",width*height);
+//            }
+//            else if(str1[0].equalsIgnoreCase("Circle")){
+//                double radius = Float.parseFloat(str1[1]);
+//                System.out.printf("Area of Circle: %.2f\n",radius*radius*Math.PI);
+//            }
+//            else {
+//                System.out.println("Invalid\n");
+//            }
+//            string = scanner.nextLine();
+//        }
+//        System.out.println("Bye\n");
+//        scanner.close();
+//    }
+//}
