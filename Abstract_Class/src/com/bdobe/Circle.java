557fd87190260a29c305e373e19bf1f86502aea1
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-11-13 17:08:01 +0900

    feat: Circle.java

--- Abstract_Class/src/com/bdobe/Circle.java
+++ Abstract_Class/src/com/bdobe/Circle.java
@@ -1,4 +1,24 @@
+package com.bdobe;
+
 public class Circle extends Shape {
     int radius;
 
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    float getLength() {
+        return (float) (2*Math.PI*radius);
+    }
+
+    @Override
+    void draw() {
+        System.out.printf("Circle, Area: %.2f, Length: %.2f\n",getArea(),getLength());
+    }
+
+    @Override
+    float getArea() {
+        return (float) (Math.PI*Math.pow(radius,2));
+    }
 }
