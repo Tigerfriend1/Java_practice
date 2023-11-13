70877da005616cc76533083bf61fc7118879d8b9
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-11-13 17:02:03 +0900

    feat: Rectangle.java

--- Abstract_Class/src/com/bdobe/Rectangle.java
+++ Abstract_Class/src/com/bdobe/Rectangle.java
@@ -1,5 +1,26 @@
+package com.bdobe;
+
 public class Rectangle extends Shape {
     int width;
     int height;
 
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    float getLength() {
+        return (width+height)*2;
+    }
+
+    @Override
+    void draw() {
+        System.out.printf("Rectangle, Area: %.2f, Length: %.2f",getArea(),getLength());
+    }
+
+    @Override
+    float getArea() {
+        return width*height;
+    }
 }
