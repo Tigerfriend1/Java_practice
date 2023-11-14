d1543e982cacdc8f3457cb8ddda0c121ccf02e25
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-11-14 14:35:49 +0900

    init : init code

---
+++ MyValidator/src/MyValidator.java
@@ -0,0 +1,19 @@
+// Validation Framework
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyValidator {
+
+    public static Set<String> validate(Object obj) {
+
+        Set<String> violations = new HashSet<>();
+
+        // Implement validation logic using Reflection
+
+        return violations;
+
+    }
+
+}
+
