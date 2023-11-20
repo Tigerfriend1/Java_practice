69913a94c7cca223bd63a350dd41869166368d35
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-11-20 15:49:53 +0900

    feat: MyRectangleSort

--- Circle_Sort_Test_Interface/src/MyRectangleSort.java
+++ Circle_Sort_Test_Interface/src/MyRectangleSort.java
@@ -1,8 +1,8 @@
-public class MySort {
+public class MyRectangleSort{
     public static void sort(MyComparable[] elements) {
         for ( int i = 0 ; i < elements.length - 1; i ++ ) {
             for ( int j = i + 1 ; j < elements.length ; j ++ ) {
-                if ( elements[i].compareTo(elements[j]) > 0 ) {
+                if ( elements[i].compareTo(elements[j]) < 0 ) {
                     MyComparable temp = elements[j] ;
                     elements[j] = elements[i] ;
                     elements[i] = temp ;
@@ -10,4 +10,6 @@ public class MySort {
             }
         }
     }
+
+
 }
