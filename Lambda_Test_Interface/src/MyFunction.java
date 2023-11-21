aad055d63f8f69d2d95065363deca2df78ce7406
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-11-21 15:34:46 +0900

    feat: MyFunction Interface

--- Lambda_Test_Interface/src/MyFunction.java
+++ Lambda_Test_Interface/src/MyFunction.java
@@ -1,2 +1,11 @@
 public interface MyFunction {
+    static MyFunction getMyFunction(String f3) {
+        MyFunction myFunction = () -> System.out.println(f3 + ".run()");
+        return myFunction;
+    }
+    void run() ;
+
+    default void sayHello(){
+        System.out.println("Hello");
+    }
 }
