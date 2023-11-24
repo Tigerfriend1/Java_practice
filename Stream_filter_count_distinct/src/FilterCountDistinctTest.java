07069068d8d096ec8254e56b784ac29e9f0b14e3
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-11-24 21:38:39 +0900

    init code

---
+++ Stream_filter_count_distinct/src/FilterCountDistinctTest.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FilterCountDistinctTest {
+
+    public static void main(String[] args) {
+
+        List<Integer> data = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+
+            data.add(sc.nextInt());
+
+        }
+
+        sc.close();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+
+}
