051a9f6f7f3c5876b60735cf84182b3708edb1f5
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-12-04 15:54:10 +0900

    feat: DNSClient.java

---
+++ InetAddress/src/DNSClient.java
@@ -0,0 +1,18 @@
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+public class DNSClient {
+
+    public static Optional<String> query(String urlString) {
+        InetAddress inetAddress;
+        try {
+            inetAddress = InetAddress.getByName(urlString);
+            return Optional.of(inetAddress.getHostAddress());
+        } catch (UnknownHostException e){
+            System.out.println(urlString+" : Name or service not known.");
+        }
+        return null;
+    }
+}
