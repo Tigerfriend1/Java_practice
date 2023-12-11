4c1eb8689b2661a56779c1f1171d124a1b22121f
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-12-11 15:55:18 +0900

    PngDownloader.java

--- URL/src/PngDownloader.java
+++ URL/src/PngDownloader.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class PngDownloader {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PngDownloader {
+    public static Path get(String url) {
+        try {
+            URL imageUrl = new URL(url);
+            String fileName = url.substring(url.lastIndexOf('/') + 1);
+            Path outputPath = Files.createTempFile(fileName, ".png");
+
+            try (InputStream in = imageUrl.openStream();
+                 FileOutputStream out = new FileOutputStream(outputPath.toFile())) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+            }
+            return outputPath;
+        } catch (IOException ex) {
+            System.err.println("Error in downloading image: " + ex.getMessage());
+            return null;
+        }
+    }
 }
